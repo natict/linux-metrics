514aec1795aa864e2cdd35f60d541708c430fef6
Nati Cohen <nati.cohen@here.com>
2017-08-31 10:10:17 +0100

    adopt memory exercises to t2.medium

--- scripts/memory/buffer.sh
+++ scripts/memory/buffer.sh
@@ -3,4 +3,4 @@
 root_dev="$(mount | grep 'on / ' | cut -d' ' -f1)"  # /dev/xvda
 
 # Read 1GB of blocks from the root device
-dd if=$root_dev of=/dev/null bs=1M count=1024
+dd if=$root_dev of=/dev/null bs=1M count=4096
--- scripts/memory/cache.sh
+++ scripts/memory/cache.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# Read all readable files in /usr
-find /usr -readable -type f -exec dd if={} of=/dev/null status=none \;
+# Read all readable files in /usr (Generates ~1.5GB cache)
+# find /usr -readable -type f -exec dd if={} of=/dev/null status=none \;
+
+# Just create a large file
+SIZE_IN_MB=4096
+dd if=/dev/zero of=/tmp/bigfile bs=1MB count=${SIZE_IN_MB}
+
+echo "Press Enter to continue..."; read
+rm /tmp/bigfile
